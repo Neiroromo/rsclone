56ad28c7530efa02b2e501ff8bef68073e7e2a10
Khakhlova <khokhlova.katherina@gmail.com>
2021-01-30 11:38:53 +0300

    feat: show  articles from api

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,3 +1,16 @@
-module.exports = (req, res) => {
-  res.render('index');
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+  try {
+    const urlAllArticles = 'http://localhost:8000/api/v1/articles/';
+    const result = await fetch(urlAllArticles);
+    const data = await result.json();
+    const allArt = data.data.articles;
+
+    res.render('main', {
+      allArticles: allArt,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
