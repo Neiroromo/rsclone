d20b46b08597dde6c961731f898c2a0e06021f3b
whattablackhole <rudolf.gritsenyak@gmail.com>
2021-02-04 11:32:50 +0300

    fix: add alert to pull req

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,8 @@ import pageRender from './page-render.js';
 import addListeners from './addEventListeners.js';
 import loginCheck from './loginCheck.js';
 
+alert('ссылка на pull-request https://github.com/Neiroromo/rsclone/pulls');
+
 loginCheck.isLogged();
 pageRender.renderNewPage('main');
 addListeners();
--- public/js/tamplates/articles-mainPage-item.js
+++ public/js/tamplates/articles-mainPage-item.js
@@ -5,7 +5,7 @@ export default function createArticleMainPageItem(
   articleID,
   imgUrl
 ) {
-  const html = `<a href="#" class="list-group-item list-group-item-action flex-column align-items-start mt-1 mb-1 mainPage-openArticle-btn" data-articleID="${articleID}">
+  const html = `<a  class="list-group-item list-group-item-action flex-column align-items-start mt-1 mb-1 mainPage-openArticle-btn" data-articleID="${articleID}">
   <div class='main-article-holder' style="display:flex; justify-content: space-between" unclickable>
     <div style="align-self: center;" unclickable>
       <div class="d-flex w-100 justify-content-between" unclickable>
