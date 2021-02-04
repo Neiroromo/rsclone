88d0518ea61fda34aba7c45aeae769b7b3eae685
whattablackhole <rudolf.gritsenyak@gmail.com>
2021-02-04 11:35:53 +0300

    fix: fix link

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ import pageRender from './page-render.js';
 import addListeners from './addEventListeners.js';
 import loginCheck from './loginCheck.js';
 
-alert('ссылка на pull-request https://github.com/Neiroromo/rsclone/pulls');
+alert('ссылка на pull-request https://github.com/Neiroromo/rsclone/pull/8');
 
 loginCheck.isLogged();
 pageRender.renderNewPage('main');
