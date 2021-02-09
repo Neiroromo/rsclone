357636497b5c13da4157a8988908c747c4eb645f
whattablackhole <rudolf.gritsenyak@gmail.com>
2021-02-09 19:06:03 +0300

    refactor: remove alert of pull request

--- public/js/index.js
+++ public/js/index.js
@@ -2,9 +2,6 @@ import pageRender from './page-render.js';
 import addListeners from './addEventListeners.js';
 import loginCheck from './loginCheck.js';
 
-alert('ссылка на pull-request https://github.com/Neiroromo/rsclone/pull/8');
-
 loginCheck.isLogged();
 pageRender.renderNewPage('main');
 addListeners();
-//пытаюсь запустить хироку
